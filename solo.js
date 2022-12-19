5a93731bd7d4e1c3eae0632937b75ed6638f39bb
ddr41g <cooler2d2@gmail.com>
2022-12-19 19:28:24 +0530

    update

--- solo.js
+++ solo.js
@@ -2,40 +2,66 @@ window.onload = function () {
     let btn = document.getElementById("jump");
     let count = 0;
 
+    //     btn.onclick = function() {
+    //     count += 1;
+    //     //changing the y position
+    //     y -= 25;
+    //     //clearing the canvas
+    //     context.clearRect(0, 0, 600, 400);
+
+    //     //redrawing the circle   
+    //     context.beginPath();
+    //     context.arc(x, y, 50, 0, 2 * Math.PI);
+    //     context.fillStyle="red";
+    //     context.fill();
+
+    //     //drawing the count value
+    //     context.font = '25px Arial';
+    //     context.fillStyle = 'white';
+    //     context.fillText("Count: " + count, 20, 30);
+    // }
+
     btn.onclick = function () {
         count += 1;
-    }
-}
+        y -= 25;
+        // x+=25
+        function draw() {
+            context.clearRect(0, 0, 600, 400);
 
-window.onload = function () {
+            context.beginPath();
+            context.arc(x, y, 50, 0, 2 * Math.PI);
+            context.fillStyle = "red";
+            context.fill();
+
+            context.font = '25px Arial';
+            context.fillStyle = 'white';
+            context.fillText("Count: " + count, 20, 30);
+        }
+        // function redraw() {
+        //     context.clearRect(0, 0, 600, 400);
+        //     y += 25;
+            
+        //     context.beginPath();
+        //     context.arc(x, y, 50, 0, 2 * Math.PI);
+        //     context.fillStyle = "red";
+        //     context.fill();
+
+        // }
+        window.requestAnimationFrame(draw);
+        // setTimeout(redraw,500);
+    }
+
     var canvas = document.getElementById("canvas");
     var context = canvas.getContext("2d");
-
-    context.beginPath();
-    context.moveTo(0, 0);
-    context.lineTo(600, 400);
-    context.moveTo(600, 0);
-    context.lineTo(0, 400);
-    context.moveTo(150,250);
-    context.rect(150,100,300,200);
-    
-    //context.beginPath();
-    // context.arc(150,100,50,0.5*Math.PI,0,1);
-    context.lineWidth=20;
-    context.strokeStyle="yellow";
-    context.stroke();
-    context.fillStyle="blue";
-    context.fill();
-
-    context.beginPath();
     var x = 300;
     var y = 350;
 
     context.arc(x, y, 50, 0, 2 * Math.PI);
-    context.fillStyle="#111111";
+    context.fillStyle = "red";
     context.fill();
 
-    context.font="42px Arial";
-
-    context.fillText("READY...",150,80);
-};
+    // context.font='25px Arial';
+    // context.fillStyle='black';
+    // context.fillText("FPS: "+ x,20,30);
+    // window.requestAnimationFrame(drawfunction);
+}
--- trial.js
+++ trial.js
@@ -20,4 +20,82 @@ window.onload = function () {
         yf = 3 * yf / 4;
 
     }
+
+
 };
+
+// from main
+
+window.onload = function () {
+    let btn = document.getElementById("jump");
+    let count = 0;
+
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+
+    context.beginPath();
+    context.moveTo(0, 0);
+    context.lineTo(600, 400);
+    context.moveTo(600, 0);
+    context.lineTo(0, 400);
+    context.moveTo(150,250);
+    context.rect(150,100,300,200);
+
+    btn.onclick = function() {
+        count += 1;
+        //changing the y position
+        y -= 25;
+        //clearing the canvas
+        context.clearRect(0, 0, 600, 400);
+         
+        //redrawing the circle   
+        context.beginPath();
+        context.arc(x, y, 50, 0, 2 * Math.PI);
+        context.fillStyle="red";
+        context.fill();
+    
+        //drawing the count value
+        context.font = '25px Arial';
+        context.fillStyle = 'white';
+        context.fillText("Count: " + count, 20, 30);
+    }
+}
+
+// window.onload = function () {
+//     var canvas = document.getElementById("canvas");
+//     var context = canvas.getContext("2d");
+
+//     context.beginPath();
+//     context.moveTo(0, 0);
+//     context.lineTo(600, 400);
+//     context.moveTo(600, 0);
+//     context.lineTo(0, 400);
+//     context.moveTo(150,250);
+//     context.rect(150,100,300,200);
+    
+//     //context.beginPath();
+//     // context.arc(150,100,50,0.5*Math.PI,0,1);
+//     context.lineWidth=20;
+//     context.strokeStyle="yellow";
+//     context.stroke();
+//     context.fillStyle="blue";
+//     context.fill();
+
+//     context.beginPath();
+//     var x = 300;
+//     var y = 350;
+
+//     context.arc(x, y, 50, 0, 2 * Math.PI);
+//     context.fillStyle="#111111";
+//     context.fill();
+
+//     context.font="42px Arial";
+
+//     context.fillText("READY...",150,80);
+//     context.strokeStyle="green";
+//     context.fillStroke("READY...",150,80);
+
+//     var img=new Image();
+//     img.src='spidey.jpg';
+//     context.drawImage(img,150,100,300,200);
+// };
